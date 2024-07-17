6e7ff861ce48c35eefb1c81b6769dba5b25d52ed
Sarmad Gardezi <mail@sarmadgardezi.com>
2024-07-17 13:20:14 +0500

    always dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,7 @@ import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   if (typeof window !== "undefined") {
-    const isDarkSet = localStorage.theme === "dark";
-    const isThemeStored = "theme" in localStorage;
-    const isDarkPrefered = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-
-    if (isDarkSet || (!isThemeStored && isDarkPrefered)) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+    document.documentElement.classList.add("dark");
   }
 
   return (
