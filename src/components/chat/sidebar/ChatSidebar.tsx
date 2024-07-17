9713a593fc00d8162fb312318461a5fe2f5d7bd0
Sarmad Gardezi <mail@sarmadgardezi.com>
2024-07-17 13:00:12 +0500

    new test change

--- src/components/chat/sidebar/ChatSidebar.tsx
+++ src/components/chat/sidebar/ChatSidebar.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { MdAdd, MdDeleteOutline, MdBuild } from "react-icons/md";
@@ -22,16 +21,20 @@ export default function ChatSidebar({}: Props) {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleNewChat = () => {
+    router.push('/gpt/').then(() => router.reload());
+  };
+
   return (
     <div className="dark left-0 top-0 h-full max-h-screen flex-col bg-gray-900 text-primary md:fixed md:flex md:w-[260px]">
       <div className="flex h-full flex-col items-stretch p-2">
-        <Link
-          href="/gpt/"
+        <button
+          onClick={handleNewChat}
           className="flex items-center gap-3 rounded border border-white/20 p-4 transition-colors bg-gradient-to-r from-purple-500 to-indigo-500"
         >
           <MdAdd />
           New chat
-        </Link>
+        </button>
 
         <Conversations />
 
