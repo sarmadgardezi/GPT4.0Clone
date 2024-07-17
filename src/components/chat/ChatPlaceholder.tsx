9743e302b4718ccb82ec082a696511984ea5c6d8
Sarmad Gardezi <mail@sarmadgardezi.com>
2024-07-17 13:58:19 +0500

    update

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -7,6 +8,7 @@ export default function ChatPlaceholder({}: Props) {
     <div className="flex h-full bg-dk w-full items-center justify-center">
       <div className="max-w-3xl p-4 text-center text-primary">
       <h1 className="text-7xl bricolage font-bold bg-gradient-heading text-center">Chatbot by OpenAi</h1> 
+      
         <p className="mt-4 text-lg">
          Use Free GPT 3.5 Turbo to generate human-like text.
         </p>
